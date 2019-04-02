23083ab08a5d8aa1e0724b65c9700ce11451ba9f
Eddy Nguyen <eddeee.nguyen@gmail.com>
2019-04-02 19:24:17 +1100

    Add typings to make exercises simpler

--- src/common/hooks/useForm/useForm.ts
+++ src/common/hooks/useForm/useForm.ts
@@ -1,3 +1,24 @@
-function useForm() {}
+import { ChangeEvent, useState } from 'react';
+
+interface State {
+  [key: string]: any;
+}
+
+interface InputProp {
+  name: string;
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+type InputPropsObject<S> = { [key in keyof S]?: InputProp };
+
+function useForm<S extends State>(initialValues: S): [S, InputPropsObject<S>] {
+  // @ts-ignore
+  const [values, setValues] = useState<S>(initialValues);
+
+  const inputs: InputPropsObject<S> = {};
+
+  return [values, inputs];
+}
 
 export default useForm;
